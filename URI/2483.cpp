1a374a449f5ffb0e0a6fd78c7c80156432a6a2ca
Luis Henrique <luishng123@gmail.com>
2017-08-22 14:22:38 -0300

    URI 2483 Feliz Nataaal!

---
+++ URI/2483.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double n;
+
+    while(scanf("%lf", &n) != EOF){
+        cout << "Feliz nat" << string(n, 'a') << "l!\n";
+    }
+
+    return 0;
+}
