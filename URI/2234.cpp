54bd4169137b822fc3bb33e71c5304ea7d2107a4
Luis Henrique <luishng123@gmail.com>
2017-08-23 08:28:48 -0300

    URI 2234 Cachorros-Quentes

---
+++ URI/2234.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+    double h;
+    double p; 
+    double result;
+
+    scanf("%lf %lf", &h, &p);
+
+    result = h/p;
+
+    printf("%.2lf\n", result);
+
+    return 0;
+}
