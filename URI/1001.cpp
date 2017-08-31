0149b3e368b7298ed9e69de896da94fc096d061a
Luis Henrique <luishng123@gmail.com>
2017-08-31 09:09:45 -0300

    URI 1001 Extremamente Básico

---
+++ URI/1001.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int num1, num2;
+
+    scanf("%d %d", &num1, &num2);
+
+    printf("X = %d\n", num1+num2);
+
+    return 0;
+}
