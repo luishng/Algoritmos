5490c89edede947a650f022fce253f2ea72dd7c6
Luis Henrique <luishng123@gmail.com>
2017-08-31 09:18:51 -0300

    URI 1002 Área do Círculo

---
+++ URI/1002.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double entrada;
+    double const PI = 3.14159;
+
+    scanf("%lf", &entrada);
+
+    printf("A=%.4lf\n", PI*pow(entrada, 2));
+
+    return 0;
+}
