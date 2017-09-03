d57056947d162ca68572afc8fc34610841c22d70
Luis Henrique <luishng123@gmail.com>
2017-09-03 11:14:06 -0300

    URI 1196 WERTYU

---
+++ URI/1196.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() { 
+    string teclado = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./"; 
+    string codigo;
+    
+
+    while(getline(cin, codigo)){
+        string descriptografado = "";
+
+        for(int i = 0; i < codigo.size(); i++){
+            if(codigo[i] == ' '){
+                descriptografado += ' ';
+            }else{
+                int posicao = teclado.find(codigo[i]);
+                descriptografado += teclado[posicao-1];
+            }
+        }
+
+        cout << descriptografado << endl;
+    }
+    
+    return 0;
+}
